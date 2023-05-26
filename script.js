81ccd8ff9daf7a27c4cdb55e02e0757a821770b2
Parth0415 <parthmali1597@gmail.com>
2023-05-25 23:36:35 -0400

    Start js by getting Element by ID from index.html

---
+++ script.js
@@ -0,0 +1,26 @@
+let searchedCity = document.getElementById("searchedCity"); 
+
+let searchButton = document.getElementById("searchButton");
+
+let cityName = document.getElementById("cityName");
+
+let cityDate = document.getElementById("cityDate");
+
+let icon = document.getElementById("icon");
+
+let headerTemp = document.getElementById("headerTemp");
+
+let headerWind = document.getElementById("headerWind");
+
+let headerHumidity = document.getElementById("headerHumidity");
+
+let cardTemp = document.getElementById("cardTemp");
+
+let cardWind = document.getElementById("cardWind");
+
+let cardHumidity = document.getElementById("cardHumidity");
+
+const APIKEY = "e35552a8d778748871c9efe011a06edf";
+
+const url = "api.openweathermap.org/data/2.5/forecast?lat=&lon=&appid="
+
